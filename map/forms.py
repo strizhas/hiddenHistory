da61de7772ae3c5eaf006b989140a59f63b5fb66
Андрей <STR@Andrejs-MacBook-Pro.local>
2020-05-19 23:49:40 +0300

    initial commit

--- map/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from location_field.forms.plain import PlainLocationField
-
-
-class PhotoForm(forms.Form):
-    location = forms.CharField(help_text="город")
-    position = PlainLocationField(based_fields=['location'], initial='-22.2876834,-49.1607606')
-    img = forms.ImageField()
