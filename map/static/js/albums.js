55423932eff3d07128cf1dcb355d735482a6159c
Андрей <STR@Andrejs-MacBook-Pro.local>
2020-06-11 10:30:35 +0300

    style fixes

--- map/static/js/albums.js
+++ map/static/js/albums.js
@@ -1,6 +1,6 @@
 
 var base_url = location.href.split('?')[0];
-
+var w = null;
 $( document ).ready(function() {
 
     $('.album-photo-preview').on('click', function(e) {
@@ -11,35 +11,58 @@ $( document ).ready(function() {
         p_id = p_id[p_id.length - 1]
         change_url(p_id)
 
+
         $.get(url).done(function(data) {
-            $('<div>', {
-                'id': 'ajax-content',
-                'class': 'ajax-content-window',
-                'html': data
-            }).appendTo('#content-wrapper')
-
-
-            $('#go-back-link').on('click', function(e) {
-                e.preventDefault();
-                remove_ajax_window()
-            })
+            w = new AjaxWindow()
+            w.build(data)
         });
     })
 
     window.onpopstate =  function(e) {
         e.preventDefault()
         if ($('#ajax-content').length) {
-            remove_ajax_window()
+            w.remove_ajax_window()
         } else {
             window.history.back()
         }
     }
 })
 
-function remove_ajax_window() {
-    $('#ajax-content').remove();
-    window.history.pushState( {} , '', base_url );
+function AjaxWindow() {
+
+    this.header_size = 70;
+    this.build = function(data) {
+
+        $('<div>', {
+            'id': 'ajax-content',
+            'class': 'ajax-content-window',
+            'html': data
+        }).appendTo('#content-wrapper')
+
+
+        this.bind_events()
+
+    }
+    this.bind_events = function() {
+        var _this = this
+        $('#go-back-link').on('click', function(e) {
+            e.preventDefault();
+            _this.remove_ajax_window()
+        })
+
+        $('#close-window-button').on('click', function(e) {
+            e.preventDefault();
+            _this.remove_ajax_window()
+        })
+    }
+
+    this.remove_ajax_window = function() {
+        $('#ajax-content').remove();
+        window.history.pushState( {} , '', base_url );
+    }
 }
+
+
 
 function change_url(p_id) {
     var url = base_url + `?show=${p_id}`;
