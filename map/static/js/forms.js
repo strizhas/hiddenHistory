fd0324d2ca080ac11dde28e8973974794c3dd7d2
Стрижков Андрей Викторович <strizhkov_a@tass.ru>
2020-12-26 13:40:18 +0300

    добавил progress bar для загрузки фотографий

--- map/static/js/forms.js
+++ map/static/js/forms.js
@@ -1,5 +1,30 @@
 sending = false
 
+function _(el) {
+  return document.getElementById(el);
+}
+
+function progressHandler(event) {
+  _("loaded_n_total").innerHTML = "Uploaded " + event.loaded + " bytes of " + event.total;
+  var percent = (event.loaded / event.total) * 100;
+  _("progressBar").value = Math.round(percent);
+  _("status").innerHTML = Math.round(percent) + "% uploaded... please wait";
+}
+
+function completeHandler(event) {
+  // _("status").innerHTML = event.target.responseText;
+  _("progressBar").value = 0; //wil clear progress bar after successful upload
+}
+
+function errorHandler(event) {
+  _("status").innerHTML = "Upload Failed";
+
+}
+
+function abortHandler(event) {
+  _("status").innerHTML = "Upload Aborted";
+}
+
 const submitUpdateForm =  function(event, form, callback) {
     event.preventDefault();
 
@@ -13,6 +38,7 @@ const submitUpdateForm =  function(event, form, callback) {
     var url = $(form).attr('action');
 
     var formData = new FormData(form);
+    var progressBar = null;
 
     $.ajax({
         url : url,
@@ -20,10 +46,39 @@ const submitUpdateForm =  function(event, form, callback) {
         processData: false,
         contentType: false,
         data : formData,
+        xhr: function() {
 
+            var xhr = new window.XMLHttpRequest();
+
+            if (progressBar == null) {
+                progressBar = $("<div>", {
+                    "class": "form-progress-bar-section",
+                    "html": '<progress id="progressBar" value="0" max="100"></progress>'
+                }).appendTo($(form))
+
+                $("<div>", {
+                    "id": "loaded_n_total",
+                    "class": "upload-bytes-counter help-text"
+                }).appendTo(progressBar);
+
+                $("<div>", {
+                    "id": "status",
+                    "class": "upload-status"
+                }).appendTo(progressBar);
+            }
+
+            xhr.upload.addEventListener("progress", progressHandler, false);
+            xhr.addEventListener("load", completeHandler, false);
+            xhr.addEventListener("error", errorHandler, false);
+            xhr.addEventListener("abort", abortHandler, false);
+
+            return xhr;
+          },
         success : function(response) {
             messageDiv.style.display = "block";
             messageDiv.innerHTML = response['message'];
+
+            $(form).find("input[type='file']").val('')
 
             if (typeof(callback) == 'function') {
                 callback(response)
